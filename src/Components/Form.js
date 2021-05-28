e9811e6c1fffc82715817353b4a58aa260c07eb9
yepome <yvesepomengamini@gmail.com>
2021-05-28 23:34:17 +0200

    This is the initialization of the ToDo-List frontend App

---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoList from './TodoList'
+
+const Form = () => {
+    return (
+        <div>
+            <TodoList />
+        </div>
+    )
+}
+
+export default Form
